ad886936a9f9786cb9b316c45ea545bfcfbedf1f
Murat Dikmen <mrtdkmn@gmail.com>
2020-05-28 18:29:28 -0400

    added shap values

--- main.py
+++ main.py
@@ -29,9 +29,11 @@ app.add_middleware(
 )
 
 model = pickle.load(open('model.pickle', 'rb'))
+shap_values = pickle.load(open('shap_values.pickle', 'rb'))
 
 
 class Data(BaseModel):
+    loan_id: float
     loan_amnt: float
     mths_since_recent_inq: float
     term: str
@@ -56,28 +58,32 @@ class Data(BaseModel):
 def predict(data: Data):
     arr = []
     for key, value in data:
+        if key != = loan_id:
         arr.append(value)
+        # arr.append(value)
     # ja = [[35000.0, 4.0, "60 months", 3.0, 2.0, 65.1, 19167.0, 74.7, 3.0,
     #        56633.0, 0.0, 10.0, 705.0, 540.0, 4.0, 42.9, 15.0, 226.0, 23.4]]
     ja = [arr]
-    dfd = pd.DataFrame(ja, columns=['loan_amnt',
-                                    'mths_since_recent_inq',
-                                    'term',
-                                    "inq_last_12m",
-                                    "inq_last_6mths",
-                                    "revol_util",
-                                    "bc_open_to_buy",
-                                    "bc_util",
-                                    "num_rev_tl_bal_gt_0",
-                                    "revol_bal",
-                                    "delinq_2yrs",
-                                    "num_op_rev_tl",
-                                    "fico_range_low",
-                                    "sec_app_fico_range_low",
-                                    "mo_sin_rcnt_rev_tl_op",
-                                    "percent_bc_gt_75",
-                                    "num_rev_accts",
-                                    "mths_since_last_delinq"])
-    prediction = model.predict_proba(dfd)[0][1]
-    return prediction
-    # return {"prediction": int(prediction[0])}
+    df = pd.DataFrame(ja, columns=['loan_amnt',
+                                   'mths_since_recent_inq',
+                                   'term',
+                                   "inq_last_12m",
+                                   "inq_last_6mths",
+                                   "revol_util",
+                                   "bc_open_to_buy",
+                                   "bc_util",
+                                   "num_rev_tl_bal_gt_0",
+                                   "revol_bal",
+                                   "delinq_2yrs",
+                                   "num_op_rev_tl",
+                                   "fico_range_low",
+                                   "sec_app_fico_range_low",
+                                   "mo_sin_rcnt_rev_tl_op",
+                                   "percent_bc_gt_75",
+                                   "num_rev_accts",
+                                   "mths_since_last_delinq"])
+    prediction = model.predict_proba(df)[0][1]
+    shap_vals = shap_values[loan_id]
+    data = {"prediction": prediction, "shap_vals": shap_vals}
+    return data
+    # return prediction
